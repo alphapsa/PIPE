d3c6dc4cc2e5157366385920b28f82249476448c
Alexis Brandeker <alexis@astro.su.se>
2021-03-11 10:50:22 +0100

    Cast float indices to int in read.imagette_offset

--- pipe/read.py
+++ pipe/read.py
@@ -236,10 +236,10 @@ def imagette_offset(filename, frame_range = None):
     to full array, second offset is relative to subarray
     """
     with fits.open(filename) as hdul:
-        x_off = hdul[2].data['X_OFF_FULL_ARRAY'][0]
-        y_off = hdul[2].data['Y_OFF_FULL_ARRAY'][0]
-        x_sa_off = hdul[2].data['X_OFF_SUB_ARRAY'][0]
-        y_sa_off = hdul[2].data['Y_OFF_SUB_ARRAY'][0]
+        x_off = int(hdul[2].data['X_OFF_FULL_ARRAY'][0])
+        y_off = int(hdul[2].data['Y_OFF_FULL_ARRAY'][0])
+        x_sa_off = int(hdul[2].data['X_OFF_SUB_ARRAY'][0])
+        y_sa_off = int(hdul[2].data['Y_OFF_SUB_ARRAY'][0])
         return (x_off, y_off), (x_sa_off, y_sa_off)
     raise Exception('[imagette_offset] Error: {:s} not found'.format(filename))
 
