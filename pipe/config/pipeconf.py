270f56ccefb8728227f03651abb9f8a351db541c
Jayshil <jayshilpatel147@gmail.com>
2024-02-05 16:40:43 +0100

    Paths

--- pipe/config/pipeconf.py
+++ pipe/config/pipeconf.py
@@ -2,14 +2,31 @@ import json
 import os
 import astropy.config as astropyconfig
 
-
 def cache_dir():
     return astropyconfig.get_cache_dir()
 
 
-def get_conf_paths():
+def get_conf_paths(overwrite=False):
     # Get configuration information from setup.cfg
     conf_path = os.path.join(os.path.dirname(__file__), 'conf.json')
+    
+    if (not os.path.isfile(conf_path))|(overwrite):
+        # First creating a file
+        data_path = input('Please enter a path to the data files\nPress ENTER to use the home directory\n(a directory called cheops-pipe will be created inside the home directory): ')
+        ref_path = input('Please enter a path to the calibration files\nPress ENTER to use the home directory\n(a directory called cheops-pipe will be created inside the home directory): ')
+        if data_path == '':
+            data_path = os.path.expanduser('~') + '/cheops-pipe/Data'
+        if ref_path == '':
+            ref_path = os.path.expanduser('~') + '/cheops-pipe/Ref'
+        fconf = open(os.path.dirname(__file__) + '/conf.json', 'w')
+        fconf.write('{\n')
+        fconf.write('\t"data_root" : "' + data_path + '",\n')
+        fconf.write('\t"ref_lib_data" : "' + ref_path + '"\n')
+        fconf.write('}')
+        fconf.close()
+        # And loading it
+        conf_path = os.path.join(os.path.dirname(__file__), 'conf.json')
+
     with open(conf_path, 'r') as confstream:
         confparse = json.load(confstream)
     data_root_cfg = confparse.get("data_root")
